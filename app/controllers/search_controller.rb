47077f22aeb16f2f46dde670471a2bd742fbcfbf
Elijah Fetzer <emfetz@gmail.com>
2012-03-12 20:20:01 -0400

    spellcheck revision

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @zip_code = ZipCode.new(params[:zip_code]) if params[:zip_code].present?
-    @spellcheck = BingSpell.new(params[:query]) if params[:query].present?
+    @spellcheck = Spellcheck.new(params[:query]) if params[:query].present?
   end
   
   def search
--- app/models/spellcheck.rb
+++ app/models/spellcheck.rb
@@ -1,19 +1,19 @@
 require 'savon'
 
-class BingSpell
+class Spellcheck
   attr_reader :value
   
   def initialize(query)
     # Gyoku.convert_symbols_to :camelcase #Use this method when your symbols need a capitalized first letter when converted to XML
     
-    client_bing = Savon::Client.new do
+    client = Savon::Client.new do
       wsdl.document = "http://api.bing.net/search.wsdl?AppID=C46A8496D244DC5CDFA135AD3791C2F6657D56BE&Version=2.2"
       wsdl.endpoint = "http://api.bing.net/soap.asmx"
       wsdl.namespace = "http://schemas.microsoft.com/LiveSearch/2008/03/Search"
       # wsdl.soap_actions = [:search]
     end # end of "Savon::Client.new do"
     
-    response = client_bing.request :soap, :searchrequest, body: {
+    response = client.request :soap, :searchrequest, body: {
       "parameters" => {
         "Market" => "en-us", 
         "Query" => query, 
